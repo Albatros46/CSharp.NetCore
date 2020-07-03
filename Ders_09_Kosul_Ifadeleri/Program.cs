d33445e8cca9b53d2a995d3250da163d9819a368
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-03 08:03:36 +0300

    if_else

---
+++ Ders_09_Kosul_Ifadeleri/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_09_Kosul_Ifadeleri
+{
+    class Program
+    {// yeni dosya ve proje açmak için -> dotnet new console -o  Ders_0
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ders_10_if/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_10_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
