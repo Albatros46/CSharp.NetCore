a5024b7731b0670a704a1f2178be239adfd3b710
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-04 17:36:00 +0300

    if_else

--- Ders_09_Kosul_Ifadeleri/Program.cs
+++ Ders_09_Kosul_Ifadeleri/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.CompilerServices;
+using System;
 
 namespace Ders_09_Kosul_Ifadeleri
 {
@@ -6,7 +8,25 @@ namespace Ders_09_Kosul_Ifadeleri
     {// yeni dosya ve proje açmak için -> dotnet new console -o  Ders_0
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string username="ServetAKCADAG";
+            string password="12345";
+            string _username;
+            string _passsword;
+            Console.Write("Kullancı Adı :");
+            _username=Console.ReadLine();
+            
+             Console.Write("Parola :");
+            _passsword=Console.ReadLine();
+           
+            var IsLonggedin=(username==_username);
+
+            if(username==_username){
+                if(password==_passsword)
+                Console.WriteLine("Bilgileriniz doğru hoşgeldiniz.");
+            }else{
+                Console.WriteLine("Parola veya kullanıcı adi yanlış");
+            }
+            
         }
     }
 }
