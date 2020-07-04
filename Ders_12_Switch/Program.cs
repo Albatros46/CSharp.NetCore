c13a3f878762f3341315addb31bec1d5e57021a5
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-04 22:36:45 +0300

    kosul_switch

--- Ders_12_Switch/Program.cs
+++ Ders_12_Switch/Program.cs
@@ -6,7 +6,49 @@ namespace Ders_12_Switch
     {
         static void Main(string[] args)
         {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_0
-            Console.WriteLine("Hello World!");
+            Console.Write("Kaçıncı Aydayız :");
+            int ay=int.Parse(Console.ReadLine());
+            switch(ay){
+                case 12:
+                Console.WriteLine("ARALIK Ayı");
+                    break;
+                case 11:
+                Console.WriteLine("KASIM Ayı");
+                    break;
+                case 10:
+                Console.WriteLine("EKİM Ayı");
+                    break;
+                case 9:
+                Console.WriteLine("EYLÜL Ayı");
+                    break;
+                case 8:
+                Console.WriteLine("AĞUSTOS Ayı");
+                    break;
+                case 7:
+                Console.WriteLine("TEMMUZ Ayı");
+                    break;
+                 case 6:
+                Console.WriteLine("HAZİRAN Ayı");
+                    break;
+                 case 5:
+                Console.WriteLine("MAYIS Ayı");
+                    break;
+                 case 4:
+                Console.WriteLine("NİSAN Ayı");
+                    break;
+                 case 3:
+                Console.WriteLine("MART Ayı");
+                    break;
+                 case 2:
+                Console.WriteLine("ŞUBAT Ayı");
+                    break;
+                 case 1:
+                Console.WriteLine("OCAK Ayı");
+                    break;
+                default:
+                 Console.WriteLine("Yanlış Bilgi...");   
+                 break;
+            }
         }
     }
 }
