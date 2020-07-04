f58867414287b67421c264fac9a45893b9a14ae6
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-04 17:56:31 +0300

    if_else_if

--- Ders_10_if/Program.cs
+++ Ders_10_if/Program.cs
@@ -3,10 +3,26 @@
 namespace Ders_10_if
 {
     class Program
-    {
+    {// yeni dosya ve proje açmak için -> dotnet new console -o  Ders_0
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("1.Sayıyı Girin :");
+            int x;
+            x=int.Parse(Console.ReadLine());
+            Console.Write("2.Sayıyı Girin :");
+            int y;
+            y=int.Parse(Console.ReadLine());
+            if(x>y){
+                Console.WriteLine(x+" Sayısı "+y+" sayısından büyüktür.");
+            }else if(y>x){
+                Console.WriteLine(y+" Sayısı "+x+" sayısından büyüktür.");
+            }else if(x==y)
+            {
+                Console.WriteLine(x+" Sayısı "+y+" eşittir");
+            }else
+            {
+                Console.WriteLine("Tanımlanamadı!");
+            }
         }
     }
 }
