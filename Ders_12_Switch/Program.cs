fe765beb657a6518387cf07e03f8275a812767cd
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-04 22:18:48 +0300

    Kosul_Ifadeler_Switch

---
+++ Ders_12_Switch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_12_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_0
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
