972223cd3f5efa02ff67acc4990b2d853cd23db9
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-06 13:56:13 +0300

    Ders_17_Donguler_AsalSayi

---
+++ Ders_17_Donguler_BreakContinue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Ders_17_Donguler_BreakContinue
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Program
+    {
+        static void Main(string[] args)
+        {//Donguler : break-continue
+            string name="Servet AKÇADAĞ";
+            for (int i = 0; i < name.Length; i++)
+            {
+                if(name[i]==' ') //dongu ' ' ile karşılaştığı an duracak
+              // break;
+                continue; //' ' olduğu yeri atlayıp dongunun kaldığı yerden devam etmesini sağlayacak
+                Console.WriteLine(name[i]);
+            }
+            //------------------------------
+            Console.WriteLine("While dongusu ile-----------");
+            int x=0;
+            while (x<5)
+            {
+                if(x==3)
+                break;//3 ü gördüğü an donguden çıkar
+                Console.WriteLine($"X in degeri {x}");
+                x++;
+            }
+            Console.WriteLine("------------------------");
+            //Asal sayı uygulaması
+            Console.WriteLine("Asal sayı uygulaması");
+            bool asalmi=true;
+            Console.Write("Bir Sayı Giriniz : ");
+            int a=int.Parse(Console.ReadLine());
+            if (a==1)
+            asalmi=false;
+                
+                for (int i = 2; i < a; i++)
+                {
+                    if(a%i==0){
+                        asalmi=false;
+                        
+                        break;
+                    }
+                    
+                }
+                if (asalmi)
+                    Console.WriteLine(a+" Sayı Asaldır.");
+                 else
+                    Console.WriteLine(a+" sayı asal değildir.");
+        }
+    }
+}
