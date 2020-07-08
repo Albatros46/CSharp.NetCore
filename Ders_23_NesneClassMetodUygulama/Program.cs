8c9672e565672d19355519b5c51b1513237c1fb0
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-08 21:52:22 +0300

    Ders_22_NesneClassMetodlar

---
+++ Ders_23_NesneClassMetodUygulama/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_23_NesneClassMetodUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
