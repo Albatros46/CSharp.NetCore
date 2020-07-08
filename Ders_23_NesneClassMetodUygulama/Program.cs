7b6fcc5850a4104d242a5c9e26e092aa11abfd98
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-08 22:44:46 +0300

    Ders_23_NesneClassMetodUygulama

--- Ders_23_NesneClassMetodUygulama/Program.cs
+++ Ders_23_NesneClassMetodUygulama/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 
 namespace Ders_23_NesneClassMetodUygulama
-{
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Araba{
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+        public bool Otomatik { get; set; }
+
+        public void Start(){
+            Console.WriteLine($"{this.Marka} {this.Model} Araba Çalıştırıldı...");
+        }
+         public void Stop(){
+            Console.WriteLine($"{this.Marka} {this.Model}Araba Durdu...");
+        }
+         public void Yavasla(){
+            Console.WriteLine($"{this.Marka} {this.Model}Araba Yavasliyor...");
+        }
+         public void Hizlan(){
+            Console.WriteLine($"{this.Marka} {this.Model}Araba Hızlanıyor...");
+        }
+        public void Menu(){
+            string komut="";
+            do
+            {
+                Console.WriteLine("1-Start\n2-Stop\n3-Hızlan\n4-Yavasla\nÇ-Çıkış");
+                Console.Write("Seçiminiz: ");
+                komut=Console.ReadLine();
+                switch (komut)
+                {
+                    case "1":
+                        this.Start();
+                        break;
+                    case "2":
+                        this.Stop();
+                        break;
+                    case "3":
+                        this.Hizlan();
+                        break;
+                    case "4":
+                        this.Yavasla();
+                        break;
+                    default:
+                    
+                        Console.WriteLine("Yanlış Seçim!");
+                        break;
+                }
+            } while (komut!="Ç");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a1=new Araba(){Marka="Opel",Model="Astra",Renk="Kırmızı",Otomatik=false};
+            var a2=new Araba(){Marka="Ford",Model="Mustang",Renk="Metalik Gri",Otomatik=true};
+            a2.Menu();
+           // a1.Start();
+          //  a1.Stop();
+           // a1.Hizlan();
+            //a1.Yavasla();
         }
     }
 }
