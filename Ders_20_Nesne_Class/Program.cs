940008cf6b82d4a64c059c2b0a3014f9bd21203c
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-08 14:03:44 +0300

    Ders_20_Nesne_Class

--- Ders_20_Nesne_Class/Program.cs
+++ Ders_20_Nesne_Class/Program.cs
@@ -23,30 +23,43 @@ namespace Ders_20_Nesne_Class
             Console.WriteLine($"No:{ogrno[4]} Ad Soyad:{adsoyad[4]} Şube:{sube[4]}");
             //-------------------------------
             Console.WriteLine("-------Class türedilerek yapalım----------");
-            ogrenci ogr1=new ogrenci();
+            ogrenci ogr1=new ogrenci(); //ogrenci objesi olusturduk
             ogr1.OgrnNo=101;
             ogr1.AdSoyad="Servet AKÇADAĞ";
             ogr1.Sube="12B";
+            Console.WriteLine($"No:{ogr1.OgrnNo} Ad Soyad:{ogr1.AdSoyad} Şube:{ogr1.Sube}");
 
-            ogrenci ogr2=new ogrenci();
-            ogr2.OgrnNo=102;
-            ogr2.AdSoyad="Celal Bingöl";
-            ogr2.Sube="12B";
+            ogrenci ogr2=new ogrenci(){//ogrenci objesi olusturduk
+                OgrnNo=102,
+                AdSoyad="Celal Bingöl",
+                Sube="12B"
+            };
+            Console.WriteLine($"No:{ogr2.OgrnNo} Ad Soyad:{ogr2.AdSoyad} Şube:{ogr2.Sube}");
 
             ogrenci ogr3=new ogrenci();
             ogr3.OgrnNo=103;
             ogr3.AdSoyad="Ahmet Koza";
             ogr3.Sube="12B";
-
+            Console.WriteLine($"No:{ogr3.OgrnNo} Ad Soyad:{ogr3.AdSoyad} Şube:{ogr3.Sube}");
+     
             ogrenci ogr4=new ogrenci();
             ogr4.OgrnNo=104;
             ogr4.AdSoyad="Sefa ERGİN";
             ogr4.Sube="12B";
-
-            Console.WriteLine($"No:{ogr1.OgrnNo} Ad Soyad:{ogr1.AdSoyad} Şube:{ogr1.Sube}");
-            Console.WriteLine($"No:{ogr2.OgrnNo} Ad Soyad:{ogr2.AdSoyad} Şube:{ogr2.Sube}");
-            Console.WriteLine($"No:{ogr3.OgrnNo} Ad Soyad:{ogr3.AdSoyad} Şube:{ogr3.Sube}");
             Console.WriteLine($"No:{ogr4.OgrnNo} Ad Soyad:{ogr4.AdSoyad} Şube:{ogr4.Sube}");
+
+            //Class larin dizi ile kullanımı
+            Console.WriteLine("----Class ların Dizi ile kullanımı.");
+            ogrenci[] ogrencis=new ogrenci[4];
+            ogrencis[0]=ogr1;
+            ogrencis[1]=ogr2;
+            ogrencis[2]=ogr3;
+            ogrencis[3]=ogr4;
+            for (int i = 0; i < ogrencis.Length; i++)
+            {
+                Console.WriteLine($"No:{ogrencis[i].OgrnNo} Ad Soyad:{ogrencis[i].AdSoyad} Şube:{ogrencis[i].Sube}");
+            }
+
         }
     }
 }
