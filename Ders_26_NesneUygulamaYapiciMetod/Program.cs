3ad929777a0979215723ed14a178812dc0d73170
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-09 22:35:16 +0300

    Ders_26_NesneUygulamaYapiciMetod

---
+++ Ders_26_NesneUygulamaYapiciMetod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_26_NesneUygulamaYapiciMetod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
