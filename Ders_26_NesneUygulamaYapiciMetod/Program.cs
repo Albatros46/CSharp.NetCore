357bc0a57e5cac035a67337ddc587c5e01988225
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-09 22:44:04 +0300

    Ders_26_NesneUygulamaYapiciMetod

--- Ders_26_NesneUygulamaYapiciMetod/Program.cs
+++ Ders_26_NesneUygulamaYapiciMetod/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Ders_26_NesneUygulamaYapiciMetod
 {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Product{
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsApproved { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p1=new Product();
+            Console.WriteLine(p1.ProductId);
+            Console.WriteLine(p1.Name);
+            Console.WriteLine(p1.Price);
+            Console.WriteLine(p1.IsApproved);//Ürün Onaylı mı? kontrol için
         }
     }
 }
