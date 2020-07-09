b5bdff11bc708826103345e4fb116624d85da057
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-09 22:36:34 +0300

    Ders_26_NesneUygulamaYapiciMetod

--- Ders_26_NesneUygulamaYapiciMetod/Program.cs
+++ Ders_26_NesneUygulamaYapiciMetod/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace Ders_26_NesneUygulamaYapiciMetod
-{
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
     class Program
     {
         static void Main(string[] args)
