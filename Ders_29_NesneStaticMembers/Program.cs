8a0b5e3cf7fcf943e3fb76f9f695b885a1dce205
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-10 18:30:33 +0300

    ers_29_NesneStaticMembers

--- Ders_29_NesneStaticMembers/Program.cs
+++ Ders_29_NesneStaticMembers/Program.cs
@@ -22,18 +22,28 @@ namespace Ders_29_NesneStaticMembers
             Console.WriteLine($"Okul Adı :{School} Adres :{Adress}");
         }
     }
+    static class HelperMetods{//static class (yardımcı metod)
+        public static string KarakterDuzelt(string str){
+            return str.Replace("ç","o")
+                      .Replace("ş","s")
+                      .Replace("ğ","g")
+                      .Replace("ı","i");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var s1=new Student("Servet",101);
-            var s2=new Student("Akif",102);
-            var s3=new Student("Ahmet",103);
-            Student.DisplaySchoolDetails();
-            s1.DisplayStudentDetails();
-            s2.DisplayStudentDetails();
-            s3.DisplayStudentDetails();
-            
+            // var s1=new Student("Servet",101);
+            // var s2=new Student("Akif",102);
+            // var s3=new Student("Ahmet",103);
+            // Student.DisplaySchoolDetails();
+            // s1.DisplayStudentDetails();
+            // s2.DisplayStudentDetails();
+            // s3.DisplayStudentDetails();
+            string str="ölçme ve değerlendirme";
+            var result= HelperMetods.KarakterDuzelt(str);
+            Console.WriteLine(result);
         }
     }
 }
