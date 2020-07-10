260845b30990cb62d0849a02783c681a3b0a8abb
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-10 16:26:08 +0300

    Ders_28_NesneUygulama

--- Ders_28_NesneUygulama/Program.cs
+++ Ders_28_NesneUygulama/Program.cs
@@ -13,10 +13,40 @@ namespace Ders_28_NesneUygulama
         public string Text { get; set; }
         public string[] Choices { get; set; }//secenekler
         public string Answer { get; set; }//cevap
+        public int QuestionsIndex{ get; set; }
         public string checkAnswer(string answer){
              return this.Answer=answer;
         }
+       
         
+    }
+     class Quiz{
+            public Quiz(Question[] questions ){
+                this.Questions=questions;
+                this.QuestionIndex=0;
+            }
+            private Question[] Questions { get; set; }
+            public int QuestionIndex{get; set;}
+            public Question GetQuestion(){
+                return this.QuestionIndex[this.QuestionIndex];
+            }
+            public void DisplayQuestion(){
+                var qustion=this.GetQuestion();
+                Console.WriteLine($"{this.QuestionIndex+1}: {q.Text}");
+                foreach (var c in qustion.Choices)
+                {
+                    Console.WriteLine($"-{c}");
+                }
+                Console.Write("Cevap :");
+                var cevap=Console.ReadLine();
+                //Console.WriteLine(qustion.checkAnswer(cevap));
+                this.Guess(cevap);
+            }
+            public void Guess(string answer){
+                var qustion=this.GetQuestion();
+                Console.WriteLine();
+
+            }
     }
     class Program
     {
@@ -27,23 +57,31 @@ namespace Ders_28_NesneUygulama
             var q3=new Question("En en çok kazandıran Programlama Dili :",new string[]{"C#","Java","Python","C++"},"Java");
             
             var question=new Question[]{q1,q2,q3};
+            var quiz=new Quiz(questions);
+            quiz.DisplayQuestion();
+            Console.WriteLine(quiz.GetQuestion().Text);
+            quiz.QuestionIndex++;
+            Console.WriteLine(quiz.GetQuestion().Text);
+            quiz.QuestionIndex++;
+            Console.WriteLine(quiz.GetQuestion().Text);
+            quiz.QuestionIndex++;
 
-            Console.WriteLine(q1.checkAnswer("C#"));
-            Console.WriteLine(q2.checkAnswer("Java"));
-            Console.WriteLine(q3.checkAnswer("Python"));
-            int index=1;
-            foreach (var q in question)
-            {
-                Console.WriteLine($"Soru {index}: {q.Text}");
-                index++;
-                foreach (var c in q.Choices)
-                {
-                    Console.WriteLine($"Cevap -{c}");
-                }
-                Console.WriteLine("Cevap :");
-                var cevap=Console.ReadLine();
-                Console.WriteLine(q.checkAnswer(cevap));
-            }
+            // Console.WriteLine(q1.checkAnswer("C#"));
+            // Console.WriteLine(q2.checkAnswer("Java"));
+            // Console.WriteLine(q3.checkAnswer("Python"));
+            //int index=1;
+            // foreach (var q in question)
+            // {
+            //     Console.WriteLine($"Soru {index}: {q.Text}");
+            //     index++;
+            //     foreach (var c in q.Choices)
+            //     {
+            //         Console.WriteLine($"Cevap -{c}");
+            //     }
+            //     Console.WriteLine("Cevap :");
+            //     var cevap=Console.ReadLine();
+            //     Console.WriteLine(q.checkAnswer(cevap));
+            // }
         }
     }
 }
