780495d9d476ce875e187f81dc9faee915c0d5c1
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-11 19:56:58 +0300

    Ders_30_NesneStaticMembersUygulama

--- Ders_30_NesneStaticMembersUygulama/Program.cs
+++ Ders_30_NesneStaticMembersUygulama/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Ders_30_NesneStaticMembersUygulama
 {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Product//Product objesi olusturuldu.
+    {
+        public int ProductId { get; set; }//UrunId
+        public string  ProductName { get; set; }//UrunAdi
+        public double Price  { get; set; }//Fiyat
+        public bool  IsApproved { get; set; }//Onaylımı
+    }
+    static class ProductManager //veritabani mantığını burda gosterecegiz.
+    {
+        static Product[] Products; //public olmadiği icin disaridan erisim olmayacak metod ile erisim olacak
+        static ProductManager Program()//constructor ismi public veya static olmayacak
+        {
+            Products=new Product[5];//product dizini 5 elemanlı olacak
+            Products[0]=new Product{ProductId=1,ProductName="Iphone 7",Price=4250,IsApproved=true};
+            Products[1]=new Product{ProductId=2,ProductName="Iphone 6",Price=2850,IsApproved=false};
+            Products[2]=new Product{ProductId=3,ProductName="Iphone 7 Plus",Price=4250,IsApproved=true};
+            Products[3]=new Product{ProductId=4,ProductName="Samsung Galaksi",Price=2500,IsApproved=false};
+            Products[4]=new Product{ProductId=5,ProductName="Iphone 8 X Max",Price=7500,IsApproved=true};
+        }
+        public static Product[] GetProducts(){
+            return Products; //ProductManager deki listeye metod araciliği ile erisecegiz
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var products=ProductManager.GetProducts();
+            foreach (var p1 in products)
+            {
+               Console.WriteLine($"Urun Id:{p1.ProductId} Urun Adı :{p1.ProductName} Fiyatı :{p1.Price} Onalı Mı? {p1.IsApproved}");
+            }
         }
     }
 }
