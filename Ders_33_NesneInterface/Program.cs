6caf3a4c535a3f59c2eab95e01f8178503ff63b7
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-12 14:09:43 +0300

    Ders_33_NesneInterface

--- Ders_33_NesneInterface/Program.cs
+++ Ders_33_NesneInterface/Program.cs
@@ -1,33 +1,74 @@
 ﻿using System;
 
-namespace Ders_33_NesneInterface
+namespace Ders_33_NesneInterface //Interface Kullanımı
 {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
     interface IPersonel
     {
         string adsoyad{get; set;} //public vs gibi erişim belirteci kullanilmaz
         string adres{get; set;}
         string departman { get; set; }
+        double maas{get; set;}
         void bilgi();//gövdesiz metod tanımlariz.Interface içinde olduğundan varsayılan olarak abstract class tırlar
 
     }
     class Yonetici:IPersonel
     {
+        public Yonetici(string _adsoyad, string _adres, string _departman)
+        {
+            this.adsoyad=_adsoyad;
+            this.adres=_adres;
+            this.departman=_departman;
+        }
         public string adsoyad{get;set; }
         public string adres{get;set; }
         public string departman{get;set; }
         public void bilgi(){
-            Console.WriteLine($"{this.adsoyad} isimli personnel {this.departman} departmanında görevlidir.");
+           //Console.WriteLine($"{this.adsoyad} isimli personnel {this.departman} departmanında yöneticidir.");
+            Console.WriteLine($"Adı Soyadı:{this.adsoyad} Departmanı:{this.departman} Adresi:{this.adres}");
         }
     }
-    class Calisan
+    class Calisan:IPersonel
     {
-        
+            public Calisan(string _adsoyad, string _adres, string _departman)
+        {
+            this.adsoyad=_adsoyad;
+            this.adres=_adres;
+            this.departman=_departman;
+        }
+        public string adsoyad{get;set; }
+        public string adres{get;set; }
+        public string departman{get;set; }
+        public void bilgi(){
+            //Console.WriteLine($"{this.adsoyad} isimli personnel {this.departman} departmanında çalışandır.");
+            Console.WriteLine($"Adı Soyadı:{this.adsoyad} Departmanı:{this.departman} Adresi:{this.adres}");
+        }
+    }
+    class Robot : IPersonel
+    {
+        public Robot()
+        {
+        }
     }
     class Program//Interface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Yonetici yonetici1= new Yonetici();//Şeklinde de obje uretilebilir.
+            // Calisan calisan1=new Calisan();
+            // //--------------vaya------------
+            // IPersonel yonetici2=new Yonetici();//Şeklinde de obje uretilebilir.
+            // IPersonel calisan2=new Yonetici();
+
+            var personeller=new IPersonel[4];
+            personeller[0]=new Yonetici("Servet AKÇADAĞ","Kahramanmaraş","Bilgi İşlem");
+            personeller[1]=new Yonetici("Akif AKÇADAĞ","Kahramanmaraş","Sniper");
+            personeller[2]=new Calisan("Ahmet KOZAK","GAZİANTEP","Kalite Kontrol");
+            personeller[3]=new Calisan("Celal BİNGÖL","ANKARA","Güvenlik Şefi");
+            foreach (var p in personeller)
+            {
+                //Console.WriteLine($"Adı Soyadı:{p.adsoyad} Departmanı:{p.departman} Adresi:{p.adres}");
+                p.bilgi();
+            }
         }
     }
 }
