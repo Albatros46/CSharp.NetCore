69a81dd847d07a26a85b45044e8dc430930826a8
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-12 13:08:44 +0300

    Ders_33_NesneInterface

---
+++ Ders_33_NesneInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_33_NesneInterface
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
