7a15f035eec90191ea1e43656a5b4533afe8d18e
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-13 11:22:26 +0300

    Ders_35_Collections

---
+++ Ders_35_Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_35_Collections
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
