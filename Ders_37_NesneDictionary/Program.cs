17109edfc8985549cb5bc4030708fc91ef1ce29a
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-13 23:10:44 +0300

    Ders_37_NesneDictionary

---
+++ Ders_37_NesneDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ders_37_NesneDictionary
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Program
+    {
+        static void Main(string[] args)
+        {//dictionary
+        // Dictionary<TKey,TValue>
+            
+        }
+    }
+}
