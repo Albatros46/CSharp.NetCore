be58d25697adc95c0b81fc5c3885f13c3ac3fc21
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-13 11:45:20 +0300

    Ders_35_Collections

--- Ders_35_Collections/Program.cs
+++ Ders_35_Collections/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections;
+using System;
 
 namespace Ders_35_Collections
 {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
@@ -6,7 +7,47 @@ namespace Ders_35_Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+                Array->fixed size(sabit genişlik)
+                Collections
+                |->non-generic
+                |--->Arraylist, SortedList
+                |->generic
+                |--->int, string,product gibi degerler almaktadir.
+
+            */
+            //IList myList=new ArrayList(); //IList özellikleri daha fazla
+            //ICollection myList=new ArrayList();//ICollection özellikleri daha fazla
+            ArrayList myList=new ArrayList();
+            myList.Add(30);
+            myList.Add("Kahramanmaraş");
+            myList.Add(10.5);
+
+            ArrayList myList2=new ArrayList(){10.5,50,"Ankara"};
+            //Liste elemanlarına erişim sağlamak için 
+            //Accessing Item
+            Console.WriteLine(myList[0]);
+            Console.WriteLine(myList2[3]);
+            
+            myList.Insert(1,20);
+            myList.InsertRange(3,myList2);//3. indexten itibaren mylist2 yi ekle
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+            //Remove items - listden obje silme
+            myList.Remove(30);
+            myList.RemoveAt(2);//2.indexteki objeyi sil
+            myList.RemoveRange(0,2);//0. indexten balayarak 2 obje sil
+
+            //sıralama işlemi
+            ArrayList sayilar=new ArrayList(){1,45,32,31,43,61,16,21,26};
+            
+            foreach (var i in sayilar)
+            {
+                Console.WriteLine(i);
+            }
+            sayilar.Sort();
         }
     }
 }
