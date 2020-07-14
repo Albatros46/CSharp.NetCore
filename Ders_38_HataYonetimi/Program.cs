9e9daadb11574d8108a55b1cb1e2d7edcf1211ba
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-14 13:35:43 +0300

    Ders_38_HataYonetimi

--- Ders_38_HataYonetimi/Program.cs
+++ Ders_38_HataYonetimi/Program.cs
@@ -23,19 +23,23 @@ namespace Ders_38_HataYonetimi
              var sonuc=sayi1/sayi2;
              Console.WriteLine($"{sayi1}/{sayi2}={sonuc}",sayi1,sayi2,sonuc);
          }
-        //  catch (DivideByZeroException ex)//Exception Handling, bir sayıyı 0 a bölmek istendiğinde
-        //  {
-        //      Console.WriteLine("Hiçbir sayı sıfır(0)'a bölünemez.");
-        //      Console.WriteLine(ex.Message);
-        //  }
-        //  catch(FormatException ex)//sayı yerine harf girildiğinde
-        //  {
-        //      Console.WriteLine("Sayısal karakter dışında deger girdiniz.");
-        //      Console.WriteLine(ex.Message);
-        //  }
+          catch (DivideByZeroException ex)//Exception Handling, bir sayıyı 0 a bölmek istendiğinde
+          {
+              Console.WriteLine("Hiçbir sayı sıfır(0)'a bölünemez.");
+              Console.WriteLine(ex.Message);
+          }
+          catch(FormatException ex)//sayı yerine harf girildiğinde
+          {
+              Console.WriteLine("Sayısal karakter dışında deger girdiniz.");
+              Console.WriteLine(ex.Message);
+          }
          catch(Exception e){
              Console.WriteLine("Bir Hata Oluştu..");
              Console.WriteLine(e.Message);
+         }
+         finally{
+             //Hata olsun veya olmasın kesinlikle calişmasini istegimiz kodlar
+             Console.WriteLine("Finally Blogu calisti.");
          }
          //------------------
          // Product p=null;
