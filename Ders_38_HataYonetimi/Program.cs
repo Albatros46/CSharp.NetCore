8c7cb63d1690fab97a1b706cac3a20806dc171a3
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-14 10:00:06 +0300

    Ders_38_HataYonetimi

---
+++ Ders_38_HataYonetimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_38_HataYonetimi
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
