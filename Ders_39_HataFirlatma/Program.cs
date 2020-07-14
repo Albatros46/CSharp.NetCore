8267a0efccb64b2883e1d1a94ae26ad4bac50d8e
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-14 15:44:22 +0300

    Ders_39_HataFirlatma

--- Ders_39_HataFirlatma/Program.cs
+++ Ders_39_HataFirlatma/Program.cs
@@ -4,9 +4,16 @@ namespace Ders_39_HataFirlatma
 {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
     class Program
     {
+        static void checked_password(string pasword){
+            if (pasword.Length<8 && pasword.Length>15){
+                throw new Exception("Parola 8 karakterden az ve 15 karakterden fazla olamaz!");
+            }
+        }
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {//Exception Handling: kendi hata kodlarımızı yazalim.
+            int sayi=10;
+            if (sayi>5)
+                throw new Exception("Sayi 5 den buyuk olamaz");
         }
     }
 }
