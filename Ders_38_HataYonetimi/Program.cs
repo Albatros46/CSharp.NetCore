da0222cc8281d948de44949ed8109942ed5ebb08
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-14 13:31:54 +0300

    Ders_38_HataYonetimi

--- Ders_38_HataYonetimi/Program.cs
+++ Ders_38_HataYonetimi/Program.cs
@@ -9,21 +9,37 @@ namespace Ders_38_HataYonetimi
     class Program
     {
         static void Main(string[] args)
-        {//Exception ve Exception Handling
-        //Exception
-        //Unhandled exception. System.FormatException: 
-        //Unhandled exception. System.DivideByZeroException: 
-        //Unhandled exception. System.NullReferenceException:
-            Console.Write("1.Sayı Giriniz :");
-            int sayi1=int.Parse(Console.ReadLine());
-            Console.Write("2.Sayı Giriniz :");
-            int sayi2=int.Parse(Console.ReadLine());
-            var sonuc=sayi1/sayi2;
-            
-
-            //------------------
-            Product p=null;
-            Console.Write(p.Name);//Unhandled exception. System.NullReferenceException: product nesnesi null(boş) oldugu için yazdıracak obje yok hata verir
+        {//Exception class dan türetildiği icin exception ile yakalayabiliriz.
+         //Exception
+         //Unhandled exception. System.FormatException: 
+         //Unhandled exception. System.DivideByZeroException: 
+         //Unhandled exception. System.NullReferenceException:
+         try//Hata olabilecek kodlarimizi try-catch bloklari arasina yazacagiz.
+         {
+             Console.Write("1.Sayı Giriniz :");
+             int sayi1=int.Parse(Console.ReadLine());
+             Console.Write("2.Sayı Giriniz :");
+             int sayi2=int.Parse(Console.ReadLine());
+             var sonuc=sayi1/sayi2;
+             Console.WriteLine($"{sayi1}/{sayi2}={sonuc}",sayi1,sayi2,sonuc);
+         }
+        //  catch (DivideByZeroException ex)//Exception Handling, bir sayıyı 0 a bölmek istendiğinde
+        //  {
+        //      Console.WriteLine("Hiçbir sayı sıfır(0)'a bölünemez.");
+        //      Console.WriteLine(ex.Message);
+        //  }
+        //  catch(FormatException ex)//sayı yerine harf girildiğinde
+        //  {
+        //      Console.WriteLine("Sayısal karakter dışında deger girdiniz.");
+        //      Console.WriteLine(ex.Message);
+        //  }
+         catch(Exception e){
+             Console.WriteLine("Bir Hata Oluştu..");
+             Console.WriteLine(e.Message);
+         }
+         //------------------
+         // Product p=null;
+         // Console.Write(p.Name);//Unhandled exception. System.NullReferenceException: product nesnesi null(boş) oldugu için yazdıracak obje yok hata verir
         }
     }
 }
