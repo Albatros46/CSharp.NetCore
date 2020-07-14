48597411785c8e80af6d3afc3660eb06b1d116b5
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-14 14:48:41 +0300

    Ders_39_HataFirlatma

---
+++ Ders_39_HataFirlatma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_39_HataFirlatma
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
