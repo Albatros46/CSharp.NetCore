5c1b73d6a00d75e371ce98222f1e92fd280d25f7
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-15 23:04:35 +0300

    Ders_41_KendiHataSinifimiz

--- Ders_41_KendiHataSinifimiz/Program.cs
+++ Ders_41_KendiHataSinifimiz/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Ders_41_KendiHataSinifimiz
 {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+   //Exception
+    class LoginException:Exception{
+        public LoginException(string message):base(message)
+        {
+            //LoginException Exception türetildiği için Exception tüm özelliklerine sahiptir.
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Login("ServetAKÇADAĞ","12345634");
+                Console.WriteLine("Giriş Başarlı...");
+            }
+            catch (LoginException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+        static void Login(string username, string password){
+            if (username.Contains(" "))
+                throw new LoginException("Username boşluk içeremez.");
+            
+            if(password.Length<8)
+                throw new LoginException("Parola 8 karakterden az olamaz!");
         }
     }
 }
