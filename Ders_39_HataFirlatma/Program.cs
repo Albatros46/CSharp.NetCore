1a2d99f5ee09a453e5e3f6b65243258742b19d59
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-15 12:42:19 +0300

    Ders_40_HataFirlatmaUygulama

--- Ders_39_HataFirlatma/Program.cs
+++ Ders_39_HataFirlatma/Program.cs
@@ -1,19 +1,66 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 
 namespace Ders_39_HataFirlatma
 {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Person{
+        public string _name;
+        public string Name{
+            get{
+                return _name;
+            }
+            set{
+                if (value.Length>15)
+                {
+                    throw new Exception("Ad alanı için en fazla 15 karakter girebilirsiniz!");
+                }
+                _name=value;
+            }
+        }
+
+    }
     class Program
     {
-        static void checked_password(string pasword){
-            if (pasword.Length<8 && pasword.Length>15){
+        static void checked_password(string password){
+            if (password.Length<8 || password.Length>15){
                 throw new Exception("Parola 8 karakterden az ve 15 karakterden fazla olamaz!");
+                if (!password.Any(char.IsDigit))//Any ile her karakter kontorl edilecek bir karakterin rakam olmasını kontrol edeceğiz.
+                {
+                    throw new Exception("Parola en az bir tane rakam içermelidir!");
+                }
+                if (!password.Any(char.IsLetter))//Any ile her karakter kontorl edilecek bir karakterin harf olmasını kontrol edeceğiz.
+                {
+                    throw new Exception("Parola en az bir tane harf içermelidir!");
+                }
             }
         }
         static void Main(string[] args)
         {//Exception Handling: kendi hata kodlarımızı yazalim.
-            int sayi=10;
-            if (sayi>5)
-                throw new Exception("Sayi 5 den buyuk olamaz");
+            // int sayi=10;
+            // if (sayi>5)
+            //     throw new Exception("Sayi 5 den buyuk olamaz");
+             Console.Write("Parolanızı Girin :");
+             string parola=Console.ReadLine();
+            try
+            {
+                checked_password(parola);
+                Console.WriteLine("Parola Geçerli");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);   
+            }
+
+            try
+            {
+                 var p=new Person();
+                 p.Name="Servet                                          ";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }
---
+++ Ders_40_HataFirlatmaUygulama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ders_40_HataFirlatmaUygulama
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+/*
+    1,"2","3a",abc
+    1-Listeden sayılsal ifadeleri bulunuz.
+    2-Kullanıcı q degerini girmedikçe sayısal degeri bulana kadar hata mesajı verecek.
+    3-Girilen parola içinde türkçe karakter araması yapacak
+*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
