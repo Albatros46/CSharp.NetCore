ffb250a35823719466b5e4f7e7ca41951e794a12
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-21 18:48:38 +0300

    Ders_42_MsSQL_Baglanti

---
+++ Ders_42_MsSQL_Baglanti/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders_42_MsSQL_Baglanti
+{// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
