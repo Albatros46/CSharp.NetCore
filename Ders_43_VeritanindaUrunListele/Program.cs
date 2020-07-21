97165a28e23324c135daf7a41befeae6f619c879
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2020-07-21 21:22:57 +0300

    Ders_43_VeritanindaUrunListele

--- Ders_43_VeritanindaUrunListele/Program.cs
+++ Ders_43_VeritanindaUrunListele/Program.cs
@@ -3,23 +3,17 @@ using System.Data;
 using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
 
-namespace Ders_42_MsSQL_Baglanti
+namespace Ders_43_VeritanindaUrunListele
 {// Yeni Proje ve dosya olusturmak icin -> dotnet new console -o  Ders_
     class Program
     {
         static void Main(string[] args)
         {
-            GetSqlConnection();
-            GetMySqlConnection();
+            GetAllProducts();
         }
-        static void GetAllProducts(){
-            
-        }
-        static void GetMySqlConnection(){
-            //MySql Server e bağlantı işlemi
-            string connectionString=@"server=127.0.0.1;port=3306;database=northwind;user=root;password=12345;";
-            //consol ekranına dotnet add package MySql.Data yazıp driveri projemize ekliyoruz.
-            using (var connection=new MySqlConnection(connectionString))
+        static void GetAllProducts()
+        {
+            using (var connection=MySqlConnection())
             {
                 try
                 {
@@ -35,8 +29,14 @@ namespace Ders_42_MsSQL_Baglanti
                     connection.Close();
                 }
             }
-         
-        } 
+        }
+         static MySqlConnection GetMySqlConnection()
+        {
+            //MySql Server e bağlantı işlemi
+            //consol ekranına dotnet add package MySql.Data yazıp driveri projemize ekliyoruz.
+            string connectionString=@"server=127.0.0.1;port=3306;database=northwind;user=root;password=12345;";
+            return connection=new MySqlConnection(connectionString);
+        }
         static void GetSqlConnection(){
             //MsSQL Server e bağlanacağiz
             // @ ekleyerek \ in string ifade içinde olduğunu belirtiyoruz.
